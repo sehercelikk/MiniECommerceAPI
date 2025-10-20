e62984f2c9b00b7e702d01a8eedc8ec713654b37
asus <asus@DESKTOP-1IHTQ4L>
2025-10-20 10:26:59 +0300

    add categroy validation

---
+++ MiniECommerce.Application/Validations/CategoryValidations/AddCategoryValidation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniECommerce.Application.Validations.CategoryValidations;
+
+public class AddCategoryValidation
+{
+}
